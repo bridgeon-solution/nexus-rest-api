e060217e49f22b5353775ab9d4117523bd84243e
rifsh <rifshmuhammed@gmail.com>
2024-05-29 16:49:13 +0530

    employee creation

--- employee-service/src/respositories/employeeRepository.ts
+++ employee-service/src/respositories/employeeRepository.ts
@@ -33,7 +33,7 @@ class EmployeeRepository {
   }
 
   async findAll() {
-    const employees: Employee[] = await prisma.employee.findMany()
+    const employees: Employee[] = await prisma.employee.findMany({include:{department: true}})
     return employees
   }
 
