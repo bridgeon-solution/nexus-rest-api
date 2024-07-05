c86ccca090ff6403bfc6e360ef32ce242263aa03
rinshahan <rinshahan18@gmail.com>
2024-07-05 15:07:44 +0530

    added permission functionality

--- user-service/prisma/migrations/20240704070029_add_permission/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `enabled` on the `Permissons` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE `Permissons` DROP COLUMN `enabled`;
--- user-service/src/usecases/permissions/getPermission.usecase.ts
+++ user-service/src/usecases/permissions/getPermission.usecase.ts
@@ -1,13 +1,20 @@
 import { PrismaClient } from "@prisma/client"
 import CustomError from "../../utils/customErrorHandler"
+import employeesRepository from "../../repositories/employees.repository"
+import { Employee } from "../../enitities/entityClasses/employee.interface"
 
 const prisma = new PrismaClient()
 
 class GetPermission {
   async getpermission(employeeId: string) {
     try {
+      const employeeFind: Employee = await employeesRepository.findOne(parseInt(employeeId))
+      if (!employeeFind) {
+        throw new Error("Employee not found")
+      }
       const permission = await prisma.employeePermissions.findMany({
         where: { employeeId: parseInt(employeeId) },
+        include: { permission: true }
       })
       return permission
     } catch (error) {
